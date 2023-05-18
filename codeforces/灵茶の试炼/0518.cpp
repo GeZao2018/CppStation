a054e40afa66e2f47509b4131107e0a639172f54
gezao <2136273763@qq.com>
2023-05-18 10:07:01 +0800

    0x3f0518

--- codeforces/灵茶の试炼/0518.cpp
+++ codeforces/灵茶の试炼/0518.cpp
@@ -1,3 +1,6 @@
+// https://codeforces.com/problemset/problem/1759/G
+// 1900 2023-05-18
+
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -11,7 +14,7 @@ using namespace std;
 #define ULL unsigned long long
 #define PII pair<int, int>
 #define PLL pair<long, long>
-#define debug
+// #define debug
 
 const int INF = 0x3f3f3f3f;
 const LL LINF = 0x3f3f3f3f3f3f3f3f;
@@ -19,7 +22,26 @@ const int MOD = 1e9 + 7;
 const int N = 200010;
 
 void solve() {
-    
+    int n; cin >> n;
+    vector<int> b(n), pos(n+1);
+    for (int i = 1; i < n; i += 2) {
+        cin >> b[i];
+        pos[b[i]] = i;
+    }
+    priority_queue<int> pq;
+    for (int i = n; i > 0; i--) {
+        if (pos[i]) pq.push(pos[i]);
+        else if (pq.size()) {
+            b[pq.top()-1] = i;
+            pq.pop();
+        }
+        else {
+            cout << "-1" << endl;
+            return;
+        }
+    }
+    for (int i = 0; i < n; i++) cout << b[i] << " ";
+    cout << endl;
 }
 
 int main() {
